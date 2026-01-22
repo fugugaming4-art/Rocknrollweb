5bba63e54b6178c3d22f8b317541d83776bd0c4a
fugugaming4-art <fugugaming4@gmail.com>
2026-01-22 10:44:22 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,48 @@
+const PREFIX = ".";
+
+const commands = {
+    fun: [
+        "jailed", "wanted", "wasted", "chess", "connect4", "ttt", "rps",
+        "wordle", "2048", "ship", "tickle", "kiss", "slap", "feed", "pet",
+        "howcute", "howgay", "slots", "penis", "sigma", "pickupline", "iplookup"
+    ],
+
+    giveaway: [
+        `${PREFIX}gstart`
+    ],
+
+    mod: [
+        "mute", "unmute", "unmuteall", "kick", "warn", "ban", "unban",
+        "nick", "setprefix", "clear", "clear all", "clear bots",
+        "clear embeds", "clear files", "clear mentions", "clear images",
+        "clear contains", "clear reactions", "clear user", "clear emoji",
+        "role", "role humans", "role bots", "nuke", "lock", "unlock",
+        "hide", "unhide", "unbanall", "hideall", "unhideall",
+        "rolecolor", "roleicon", "steal", "addsticker",
+        "automod", "automod enable", "automod disable",
+        "automod punishment", "automod config", "automod logging",
+        "automod ignore", "automod ignore channel", "automod ignore role",
+        "automod ignore show", "automod ignore reset",
+        "automod unignore", "automod unignore channel", "automod unignore role"
+    ],
+
+    reports: [
+        "not rn"
+    ],
+
+    setup: [
+        `${PREFIX}ticket setup`
+    ],
+
+    utility: [
+        "afk", "avatar", "firstmessage", "servericon", "membercount",
+        "snipe", "invite", "serverinfo", "userinfo", "roleinfo",
+        "botinfo", "boosts", "ping", "list boosters", "list inrole",
+        "list emojis", "list bots", "list admins", "list invoice",
+        "list mods", "list roles", "banner user", "banner server"
+    ]
+};
+
 /**
  * 
  * 
